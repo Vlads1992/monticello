ba3837e4c3a3f7a8e34f6cb97dc5c4756a0990f6
Vlad <vladyslavmyronov@gmail.com>
2019-12-28 07:08:56 +0200

    still making the header

--- docs/js/scroll.js
+++ docs/js/scroll.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-    if (scroll >= 1) {
+    if (scroll > 160) {
         $(".header__nav").addClass("menuback");
     } else {
         $(".header__nav").removeClass("menuback");
--- docs/js/slider.js
+++ docs/js/slider.js
@@ -2,5 +2,33 @@ $('.multiple-items').slick({
   infinite: true,
   slidesToScroll: 3,
   slidesToShow: 3,
-  dots: true
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
